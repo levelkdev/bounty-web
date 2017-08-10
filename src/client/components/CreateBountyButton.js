9d30ab5a947f429da4ac7c99f225505411b828e6
Mike Calvanese <mfcalvanese@gmail.com>
2017-08-10 17:34:34 -0400

    pass config opts to bountyContracts

--- src/client/components/CreateBountyButton.js
+++ src/client/components/CreateBountyButton.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { BugBountyFactory } from 'bounty-contracts'
+import bountyContracts from 'bounty-contracts'
 
 export default () => {
   const onClickHandler = async () => {
-    const bb = await BugBountyFactory.new(
+    const { BugBounty, BugBountyFactory } = bountyContracts({
+      web3: window.web3,
+      from: window.web3.eth.accounts[0]
+    })
+    const bugBountyFactory = await BugBountyFactory.deployed()
+
+    const createTx = await bugBountyFactory.createBugBounty(
       500,
       400,
       300,
@@ -11,7 +17,12 @@ export default () => {
       100,
       'asdflkjasdflkjadsf'
     )
-    console.log('bb: ', bb)
+    console.log('tx: ', createTx)
+    const address = createTx.logs[0].args.bugBounty
+    const bugBounty = await BugBounty.at(address)
+    console.log('bugBounty: ', bugBounty)
+    const state = await bugBounty.state()
+    console.log(state.output())
   }
 
   return (
